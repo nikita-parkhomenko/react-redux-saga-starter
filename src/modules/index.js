9bad71386348ccb297c7f5559ecbafc404dcf867
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2023-02-01 20:59:03 +0200

    eslint setup

---
+++ src/modules/index.js
@@ -0,0 +1,13 @@
+
+// outsource dependencies
+import React from 'react';
+
+// local dependencies
+
+function App () {
+  return <div className="App">
+      Hello
+  </div>;
+}
+
+export default App;
